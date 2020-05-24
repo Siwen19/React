0b35442cc402f78ef470215f5d3a073526b95eda
Siwen19 <LiSiwen19@163.com>
2020-05-24 22:50:53 +0800

    add a file

---
+++ comment-book/src/index.js
@@ -0,0 +1,9 @@
+// 全局样式app
+import React from "react";
+import ReactDOM from "react-dom";
+import CommentApp from "./CommentApp";
+import "./index.css";
+ReactDOM.render(
+  <CommentApp />,
+  document.getElementById("root")
+);
