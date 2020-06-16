1ca7e76aa5317976baa2d32d05d3c47f951a800d
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-06-16 10:23:52 +0800

    Update CommentApp.js

--- comment-book/src/CommentApp.js
+++ comment-book/src/CommentApp.js
@@ -1,52 +1,31 @@
-import React, { Component } from 'react'
-import CommentInput from './CommentInput'
-import CommentList from './CommentList' 
+import React, { Component } from 'react';
+import CommentInput from './CommentInput';
+import CommentList from './CommentList';
 
 class CommentApp extends Component {
     constructor() {
-        super()
+        super();
         this.state = {
-            comments: []  /*数据状态 父组件 数据统一  一致性 */
+            comments: []
         }
-    } 
-
-    componentWillMount() {
-        this._loadComments()
-    } 
-
-    _loadComments() {
-        let comments = localStorage.getItem('comments')
-        if (comments) {
-            comments = JSON.parse(comments)
-            this.setState({ comments })
-        }
-    } 
-
-    _saveComments(comments) {
-        localStorage.setItem('comments', JSON.stringify(comments))
-    } 
-
+    }
     handleSubmitComment(comment) {
-        // console.log(comment);
-        if (!comment) return
+        // if (!comment) return 
         if (!comment.username) return alert('请输入用户名')
         if (!comment.content) return alert('请输入评论内容')
-        const comments = this.state.comments // old state
-        comments.push(comment)
-        this.setState({ comments })
-        this._saveComments(comments)
-    } 
-
+        this.state.comments.push(comment)
+        this.setState({
+            comments: this.state.comments
+        })
+    }
     render() {
         return (
             <div className='wrapper'>
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList
-                    comments={this.state.comments}
-                />
+                <CommentList comments={this.state.comments} />
             </div>
-        )
+        );
     }
-} 
+}
 
 export default CommentApp;
