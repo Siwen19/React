3cf15e0d9223d01f44d41e5aa3747c0d0c686398
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-06-18 15:50:22 +0800

    Update CommentApp.js

--- comment-book/src/CommentApp.js
+++ comment-book/src/CommentApp.js
@@ -9,6 +9,25 @@ class CommentApp extends Component {
             comments: []
         }
     }
+
+    componentWillMount() {
+        this._loadComments();
+    }
+
+    _loadComments() {
+        const comments = localStorage.getItem('comments');
+        if (comments) {
+            this.setState({
+                comments: JSON.parse(comments)
+            })
+        }
+    }
+
+    _saveComments(comments) {
+        // 存储到本地   保留上次记录
+        localStorage.setItem('comments', JSON.stringify(comments));
+    }
+
     handleSubmitComment(comment) {
         // if (!comment) return 
         if (!comment.username) return alert('请输入用户名')
@@ -17,12 +36,26 @@ class CommentApp extends Component {
         this.setState({
             comments: this.state.comments
         })
+        // 每次提交评论都会把评论列表数据保存一次
+        this._saveComments(this.state.comments);
+    }
+
+    handleDeleteComment(index) {
+        const comments = this.state.comments;
+        // 得到从 CommentList 中传过来的 index 
+        comments.splice(index, 1);
+        this.setState({ comments });
+        // 把更新后的数据同步到 localStorage 中
+        this._saveComments(comments);
     }
     render() {
+        console.log(this.state)
         return (
             <div className='wrapper'>
                 <CommentInput onSubmit={this.handleSubmitComment.bind(this)} />
-                <CommentList comments={this.state.comments} />
+                <CommentList
+                    comments={this.state.comments}
+                    onDeleteComment={this.handleDeleteComment.bind(this)} />
             </div>
         );
     }
