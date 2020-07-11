c857b42db42e10e8c1faa02c63fb760cd2dc11e8
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-11 23:35:03 +0800

    CREATE App.js

---
+++ geek-time/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import axios from 'axios';
+import { Component } from 'react';
+import './data/data';
+import Main from './main/Main';  
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
