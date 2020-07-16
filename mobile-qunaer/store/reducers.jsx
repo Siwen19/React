7a72a1323a4a4a9404250d298e3f49a8386f90a7
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-16 22:22:51 +0800

    Create reducers.jsx

---
+++ mobile-qunaer/store/reducers.jsx
@@ -0,0 +1,26 @@
+import { ACTION_SET_FROM, ACTION_SET_TO } from './actions'
+
+export default {
+    from(state = '北京', action) {
+        const { type, payload } = action
+        switch (type) {
+            case ACTION_SET_FROM:
+                return payload
+
+            default:
+                break
+        }
+        return state
+    },
+    to(state = '上海', action) {
+        const { type, payload } = action
+        switch (type) {
+            case ACTION_SET_TO:
+                return payload
+
+            default:
+                break
+        }
+        return state
+    }
+}
