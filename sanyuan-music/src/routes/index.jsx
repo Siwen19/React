e2fdba48620fb0367e864df517cba00d4349d862
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-24 21:46:13 +0800

    Update index.jsx

--- sanyuan-music/src/routes/index.jsx
+++ sanyuan-music/src/routes/index.jsx
@@ -1,28 +1,45 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { Redirect } from 'react-router-dom';
-import Recommend from '../application/Recommend/index';
+import Recommend from '../application/Recommend/';
 import BlankLayout from '../layouts/BlankLayout';
+import HomeLayout from '../layouts/HomeLayout';
+const RecommendComponent = lazy(() => import("../application/Recommend/"));
+const SingersComponent = lazy(() => import("../application/Singer/"));
+const SuspenseComponent = Component => props => {
+  return (
+    <Suspense fallback={null}>
+      <Component {...props}>
 
-export default [
+      </Component>
+    </Suspense>
+  )
+}
+export default [{
+  component: BlankLayout,
+  routes: [
     {
-        component: BlankLayout,
-        routes: [
-            {
-                path: '/',
-                exact: true,
-                render: () => <Redirect to={"/recommend"} />
-            },
-            {
-                path: '/recommend',
-                component: Recommend,
-                // routes: [
-                //     {
-                //         path: '/recommend/:id',
-                //         component: AlbumComponent
-                //     }
-
-                // ]
-            }
-        ]
+      path: "/",
+      component: HomeLayout,
+      routes: [
+        {
+          path: '/',
+          exact: true,
+          render: () => <Redirect to={"/recommend"} />
+        },
+        {
+          path: '/recommend',
+          component: SuspenseComponent(RecommendComponent)
+        },
+        {
+          path: '/singers',
+          component: SuspenseComponent(SingersComponent),
+          key: "singers"
+        },
+        // {
+        //   path: '/rank',
+        //   component: RankComponent
+        // },
+      ]
     }
-]
+  ]
+}]
