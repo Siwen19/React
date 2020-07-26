c5a85dc2f9e324d4f2b7df53f46ecfb886cc8ebb
Siwen19 <59066429+Siwen19@users.noreply.github.com>
2020-07-26 20:08:34 +0800

    Create reducer.js

---
+++ sanyuan-music/src/application/Singer/store/reducer.js
@@ -0,0 +1,19 @@
+const defaultState = {
+    category: "",
+    alpha: "",
+    singerList: [],
+    enterLoading: true,
+    // 分页
+    listOffset: 0
+}
+
+export default (state = defaultState, action) => {
+    switch (action.type) {
+        case value:
+            
+            break;
+    
+        default:
+            return state;
+    }
+}
