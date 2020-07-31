939a5dfc7b50db9d46abcdf53cf3f5ed4a870427
Siwen19 <LiSiwen19@163.com>
2020-07-31 21:09:17 +0800

    Create App.js

---
+++ transition-group/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import CSSTransitionGroupDemo from './CSSTransition';
+import Tabs from './Tab';
+
+function App() {
+  return (
+    <div>
+       <CSSTransitionGroupDemo />
+       <Tabs />
+    </div>
+  );
+}
+
+export default App;
